cc4b8162519e129d5675704a7d5a9104d4d02a33
Dageno <dageno11@gmail.com>
2017-05-05 10:00:14 +0200

    añadiendo figuritas

---
+++ src/pgn/poo/examenMarzo2017/estructuras/DimensionNoValidaException.java
@@ -0,0 +1,35 @@
+package pgn.poo.examenMarzo2017.estructuras;
+
+public class DimensionNoValidaException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DimensionNoValidaException() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public DimensionNoValidaException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DimensionNoValidaException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DimensionNoValidaException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DimensionNoValidaException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
